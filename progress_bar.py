808a0a8737e3dad485b59c33290d01ec79ea8c51
Jess Baxter <jessbaxter.dev@gmail.com>
2023-09-24 14:19:49 +1000

    Create progress_bar.py

---
+++ progress_bar.py
@@ -0,0 +1,17 @@
+from tkinter import *
+import ttkbootstrap as tb
+
+root = tb.Window(themename="superhero")
+
+# root = Tk()
+root.title("TTK Bootstrap: Progress Bar")
+# root.iconbitmap('images/codemy.ico')
+root.geometry("500x350")
+
+#
+#
+# STUFF HERE
+# Colours: primary, secondary, success, info, warning, danger, light, dark
+#
+
+root.mainloop()
