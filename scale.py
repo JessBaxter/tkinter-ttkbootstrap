362d9e5c5c1eb49260838f6cccc1bf9d21111f5f
Jess Baxter <jessbaxter.dev@gmail.com>
2023-09-24 19:25:08 +1000

    Update scale.py

--- scale.py
+++ scale.py
@@ -8,8 +8,31 @@ root.title("TTK Bootstrap")
 # root.iconbitmap('images/codemy.ico')
 root.geometry("500x350")
 
+
+# Define the scaler function
+# * Pass in e(vent), so user moving slider pushes an event into this function
+def scaler(e):
+    # Wrap the my_scale.get() function in a Python integer function
+    # * This converts floating point into whole numbers
+    my_label.config(text=f"{int(my_scale.get())}%")
+    # FLOATING POINT --> my_label.config(text=f"{my_scale.get()}")
+
+
 # Create a Scale/Slider
-my_scale = tb.Scale(root, bootstyle="warning")
+my_scale = tb.Scale(
+    root,
+    bootstyle="warning",
+    length=400,  # Physical length, not max value
+    # orient="vertical",  # default horizontal
+    from_=0,  # from_ attribute is used because from is a Python keyword
+    to=100,  # max value
+    command=scaler,
+    # state="disable",  # default 'normal'
+)
 my_scale.pack(pady=50)
+
+# Create Label
+my_label = tb.Label(root, text="", font=("Helvetica", 18))
+my_label.pack()
 
 root.mainloop()
