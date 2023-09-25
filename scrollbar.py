ad971de6c2414873c2adf0ea53efe324f91eef77
Jess Baxter <jessbaxter.dev@gmail.com>
2023-09-25 16:54:02 +1000

    Update scrollbar.py

--- scrollbar.py
+++ scrollbar.py
@@ -1,31 +1,18 @@
-# VALIDATION: https://ttkbootstrap.readthedocs.io/en/latest/api/validation/
-# VALIDATION: https://ttkbootstrap.readthedocs.io/en/latest/cookbook/validate-user-input/
-# API: https://ttkbootstrap.readthedocs.io/en/latest/api/
-
-
 from tkinter import *  # https://tcl.tk/man/tcl8.6/TkCmd/contents.htm
 import ttkbootstrap as tb  # https://ttkbootstrap.readthedocs.io/en/latest/
 
-# from datetime import date  # Allows you to set startdate in tb.DateEntry()
-# from ttkbootstrap.dialogs import Querybox  # Allows Button to bring up calendar
 
 root = tb.Window(themename="superhero")
-# LIGHT: https://ttkbootstrap.readthedocs.io/en/latest/themes/light/
-# DARK: https://ttkbootstrap.readthedocs.io/en/latest/themes/dark/
+
 
 # root = Tk()
 root.title("TTK Bootstrap")
 # root.iconbitmap('images/codemy.ico')
+# root.iconbitmap(default='images/codemy.ico')
 root.geometry("500x350")
 
-# Create XXX function
-
-# Colours: primary, secondary, success, info, warning, danger, light, dark
-# STYLE GUIDE: https://ttkbootstrap.readthedocs.io/en/latest/styleguide/
-
-# Create Label
-
 # Create Frame
+# ! EASIER TO USE ScrolledFrame here 👉 scroll_frame.py
 my_frame = tb.Frame(root)
 my_frame.pack(pady=20)
 
@@ -39,6 +26,7 @@ my_scroll = tb.Scrollbar(
 my_scroll.pack(side="right", fill="y")  # fill="y" -> y-axis
 
 # Create Text
+# ! EASIER TO USE ScrolledText here 👉 scroll_text.py
 my_text = Text(
     my_frame,
     width=30,
